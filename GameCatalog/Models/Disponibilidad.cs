ba9adc6217b08baa318359199c5a4526233cf75f
Wesley-Martinez14 <riwes1403@gmail.com>
2024-03-05 11:02:45 -0400

    agregando display en modelos

--- GameCatalog/Models/Disponibilidad.cs
+++ GameCatalog/Models/Disponibilidad.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace GameCatalog.Models;
 
 public partial class Disponibilidad
 {
     public int DisponibilidadId { get; set; }
-
+    [DisplayName("Disponibilidades")]
     public string NombreDisponibilidad { get; set; } = null!;
 
     public virtual ICollection<Juego> Juegos { get; } = new List<Juego>();
--- GameCatalog/Models/GeneroJuego.cs
+++ GameCatalog/Models/GeneroJuego.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+
 
 namespace GameCatalog.Models;
 
 public partial class GeneroJuego
 {
     public int GeneroId { get; set; }
-
+    [DisplayName("Genero")]
     public string NombreGenero { get; set; } = null!;
-
+    [DisplayName("Descripcion")]
     public string? DescripcionGenero { get; set; }
 
     public virtual ICollection<Juego> Juegos { get; } = new List<Juego>();
